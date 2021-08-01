760ea07a33d44cd457b776419d5d1c61a2eb2532
Usama Bin Rahat <53377084+UsamaBR@users.noreply.github.com>
2021-08-01 10:45:21 +0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -23,22 +23,28 @@ png("Plot4.png",
     width = 480, 
     height = 480)
 
+# Dividing the plot into two rows and two columns
 par(mfrow = c(2,2))
 
+# Plotting the first subplot
 with(newData, plot(x = dateTime, 
                    y = Global_active_power, 
                    type = "l",
                    xlab = "",
                    ylab = "Global Active Power (Kilowatts)"))
 
+# Plotting the second subplot
 with(newData, plot(x = dateTime, 
                    y = Voltage, 
                    type = "l",
                    xlab = "datetime",
                    ylab = "Voltage"))
 
+# Plotting the third subplot plot
+# Setting the margins
 par(mar = c(2,2,1,1))
 
+# Adding first layer to third subplot
 with(newData, 
      plot(x = dateTime, 
           y = Sub_metering_1, 
@@ -47,26 +53,31 @@ with(newData,
           ylab = "Energy Sub Metering",
           col = "Black"))
 
+# Adding second layer to third subplot
 with(newData, 
      lines(x = dateTime, 
            y = Sub_metering_2, 
            type = "l",
            col = "Red"))
 
+# Adding third layer to third subplot
 with(newData, 
      lines(x = dateTime, 
            y = Sub_metering_3, 
            type = "l",
            col = "Blue"))
 
+# Adding legend to third subplot
 legend("topright", 
        pch = 1, 
        lty = 1,
        col = c("Black", "Blue", "Red"),
        legend = c("Sub_Metering_1","Sub_Metering_2","Sub_Metering_3"))  
 
+# Plotting the fourth plot
 with(newData, plot(x = dateTime, 
                    y = Global_reactive_power, 
                    type = "l"))
 
+# Saving and Closing
 dev.off()
